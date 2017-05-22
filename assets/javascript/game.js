16c8b87a3139fc5219b1d4800ccf3dad915760ff
Jason H <jahenry@gwmail.gwu.edu>
2017-05-22 14:23:30 -0400

    pzychic game update package 1 Monday

---
+++ assets/javascript/game.js
@@ -0,0 +1,25 @@
+
+//Gets random letter for the psychic's choice
+function strRandomLetter(){
+			var emptyString = "";
+			var alphabet = "abcdefghijklmnopqrstuvwxyz";
+			var getRandomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+			var randomLetter = getRandomLetter;
+
+			console.log(randomLetter);
+			return randomLetter;
+}
+
+function fnIsValidChoice(chrUserChoice){
+
+	var strUserPick = chrUserChoice;
+	var	alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+	for(var i = 0; i < alphabet.length; i++)
+	{
+    	if(alphabet[i].indexOf(strUserPick) != -1){
+        console.log(strUserPick);
+        return true;
+    	}
+	}
+}
